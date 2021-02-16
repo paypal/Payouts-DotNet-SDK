261bef5af342777225433efd0ecf684a1c0db34e
aninja8 <ginerva123@gmail.com>
2021-02-16 21:30:10 +0530

    error util to parse and print error

---
+++ Samples/ErrorUtil.cs
@@ -0,0 +1,32 @@
+using PayoutsSdk.Payouts;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Samples
+{
+
+ public class ErrorUtil
+    {
+     public static Error deserializeError(string errorString){
+        var deserializedError = new Error();
+        var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(errorString));
+        var deserializer = new DataContractJsonSerializer(deserializedError.GetType());
+        deserializedError = deserializer.ReadObject(memoryStream) as Error;
+        memoryStream.Close();
+       return deserializedError;
+     }
+
+     public static void printError(Error error){
+         Console.WriteLine("Error "+error.Name);
+         Console.WriteLine("DebugId "+error.DebugId); 
+         Console.WriteLine("Message "+error.Message);
+         foreach(var detail in error.Details){
+            Console.WriteLine("Field "+detail.Field);
+             Console.WriteLine("Issue "+detail.Issue);
+         }
+     }
+
+    }
+}
